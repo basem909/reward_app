5a3fe986cb7d4f767b5faf64588cf051532dc4bc
basem909 <basemshams30@gmail.com>
2025-03-09 01:29:52 -0300

    Add relations and validations on the models

--- db/migrate/20250309034408_create_redemptions.rb
+++ db/migrate/20250309034408_create_redemptions.rb
@@ -3,7 +3,7 @@ class CreateRedemptions < ActiveRecord::Migration[7.1]
     create_table :redemptions do |t|
       t.references :user, null: false, foreign_key: true
       t.references :reward, null: false, foreign_key: true
-      t.integer :discounted_points, null: false, default: 0 
+      t.integer :discounted_points, null: false, default: 0
 
       t.timestamps
     end
